72b3d197e34cddcec91d5ba5459a3aaa53b1f636
Rahul <35rahuldshetty@gmail.com>
2021-01-06 02:43:18 +0530

    Added Model

---
+++ nn_compressor/model/model.py
@@ -0,0 +1,82 @@
+import pickle
+import numpy as np
+import tensorflow as tf
+
+class Model:
+
+    encoder = None
+    decoder = None
+
+    def __init__(self, encoder, decoder):
+        """
+        Represents an Auto-Encoder Model.
+
+        Args:
+            encoder (keras.models.Model): Encoder Model
+            decoder (keras.models.Model): Decoder Model
+        """
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def save(self, path: str):
+        """
+        Pickle and store the Keras model.
+
+        Args:
+            path (str): Path to save the model.
+        """
+        with open(path, 'wb') as fh:
+            pickle.dump(self, fh)
+
+    @staticmethod
+    def load(path:str):
+        """
+        Unpickles the saved model.
+
+        Args:
+            path (str): Path from where model has to be load
+
+        Returns:
+            [Model]: A Generic Model File
+        """
+        fh = open(path, 'rb')
+        model = pickle.load(fh)
+        return model
+
+    def save_decoder(self, path):
+        with open(path, 'wb') as fh:
+            pickle.dump(self.decoder, fh)
+
+
+    @staticmethod
+    def load_decoder(path):
+        fh = open(path, 'rb')
+        decoder = pickle.load(fh)
+        return Model(None, decoder)
+
+
+
+class KerasModel(Model):
+
+    def compress(self, data):
+        """
+        Compresses given valid content using encoder
+        """
+        assert type(data) ==  type(np.array([]))
+        assert data.shape[1:] ==  self.encoder.layers[0].output.shape.as_list()[1:]
+
+        result = self.encoder.predict(data)
+        return result
+
+    def uncompress(self, data):
+        assert type(data) ==  type(np.array([]))
+        assert data.shape[1:] ==  self.decoder.layers[0].output.shape.as_list()[1:]
+
+        result = self.decoder.predict(data)
+        return result
+
+
+        
+
+
+    
--- setup.py
+++ setup.py
@@ -10,9 +10,9 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ ]
+requirements = [ "pickle" ]
 
-setup_requirements = [ ]
+setup_requirements = [  ]
 
 test_requirements = [ ]
 
